e63899856727b4158f62fa0ae22943b55e74b77e
hamhochoisg <88182498+hamhochoisg@users.noreply.github.com>
2021-09-26 12:08:19 +0700

    webcam.py

---
+++ func.py
@@ -0,0 +1,130 @@
+import re
+import pandas as pd
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import os
+from tensorflow.keras.preprocessing import image as tfimage
+import uuid
+
+### GLOBAL Variable ###
+HOME_FOLDER = os.getcwd()
+
+#YOLO MODEL#
+MODEL = 'yolo\yolov3-face.cfg'
+WEIGHT = 'yolo\yolov3-wider_16000.weights'
+
+net = cv2.dnn.readNetFromDarknet(MODEL, WEIGHT)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+#CLASSIFIED MODEL
+MODEL_MEMBER_PREDICT_PATH = 'model\\facerec_checkpoint.h5'
+model_member_predict = tf.keras.models.load_model(MODEL_MEMBER_PREDICT_PATH)
+CLASS_NAMES = ['giang', 'hanh', 'loc', 'nam']
+
+
+def detect_bounding_box(input_img):
+    #INPUT: RAW IMAGE CAPTURE FROM WEBCAM FRAME 
+    #OUTPUT: Final boxs include all bounding box, once topleft_x, topleft_y, width, height
+
+    IMG_WIDTH, IMG_HEIGHT = 416, 416
+    blob = cv2.dnn.blobFromImage(input_img, 
+                                1/255, (IMG_WIDTH, IMG_HEIGHT),
+                                [0, 0, 0], 1, crop=False)
+    # Set model input
+    net.setInput(blob)
+
+    # Define the layers that we want to get the outputs from
+    output_layers = net.getUnconnectedOutLayersNames()
+
+    # Run 'prediction'
+    outs = net.forward(output_layers)
+
+    # Define Boundbox
+    frame = input_img.copy()
+    frame_height = frame.shape[0] #480
+    frame_width = frame.shape[1] #640
+
+    # Scan through all the bounding boxes output from the network and keep only
+    # the ones with high confidence scores. Assign the box's class label as the
+    # class with the highest score.
+
+    confidences = []
+    boxes = []
+
+    # Each frame produces 3 outs corresponding to 3 output layers
+    for out in outs:
+            # One out has multiple predictions for multiple captured objects.
+        for detection in out:
+            confidence = detection[-1]
+            # Extract position data of face area (only area with high confidence)
+            if confidence > 0.5:
+                center_x = int(round(detection[0] * frame_width))
+                center_y = int(round(detection[1] * frame_height))
+                width    = int(round(detection[2] * frame_width))
+                height   = int(round(detection[3] * frame_height))
+                #print(center_x,center_y,width,height)
+                # Find the top left point of the bounding box
+                topleft_x = center_x - width//2
+                topleft_y = center_y - height//2
+
+                #print(topleft_x, topleft_y)
+                confidences.append(float(confidence))
+                boxes.append([topleft_x, topleft_y, width, height])
+
+
+    # Perform non-maximum suppression to eliminate 
+    # redundant overlapping boxes with lower confidences.
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+    # print(len(indices))
+
+    result = frame.copy()
+    final_boxes = []
+
+    tmp_str = 'number of faces detected:' + str(len(indices))
+    for i in indices:    
+        i = i[0]
+        box = boxes[i]
+        final_boxes.append(box)
+    return final_boxes
+
+def predict_image(image_path):
+    #INPUT: IMAGE PATH
+    #OUTPUT: prediction, index, emotion
+    print('Predict function')
+
+    emotion = 'happy'
+    img_array, label = load_and_preprocess_image(image_path)
+    img_array  = np.expand_dims(img_array, axis=0) #predict nhận theo batch (1,224,224,3)
+    
+    prediction = model_member_predict.predict(img_array)
+    index = prediction.argmax()    
+    
+    return prediction[0], index, emotion
+
+def save_file_to_tmp(img):
+    tmp_path = os.path.normpath('tmp')
+    os.chdir(tmp_path)
+    id = uuid.uuid1()
+    fname_tmp = str(id) + '.jpg' 
+    cv2.imwrite(fname_tmp,img)
+    print("Image saved successful: ",fname_tmp)
+    os.chdir(HOME_FOLDER) # Trả về thư mục gốc
+    tmp_path = 'tmp\\'+ fname_tmp
+
+    return str(tmp_path)
+
+def preprocess_image(image):
+    image = tf.image.decode_png(image, channels=3)
+    image = tf.image.resize(image, [224, 224])
+    image /= 255.0  # normalize to [0,1] range
+    return image
+
+def load_and_preprocess_image(path, label=None):
+    image_raw = tf.io.read_file(path)
+    image = preprocess_image(image_raw)
+    return image, label
+
---
+++ webcam.py
@@ -0,0 +1,69 @@
+import cv2
+from func import CLASS_NAMES, detect_bounding_box
+from func import save_file_to_tmp
+from func import predict_image
+import os
+
+cap = cv2.VideoCapture(0)
+
+
+if not cap.isOpened():
+    raise IOError("Cannot open webcam")
+
+while True:
+    ret, frame = cap.read()
+
+    # frame is now the image capture by the webcam (one frame of the video)
+    cv2.imshow('Input', frame)
+    try:
+        boxes = detect_bounding_box(frame)
+        # print(boxes)
+        x_left, y_top, width, height = boxes[0]
+
+        #vẽ bounding box lên cái frame
+        cv2.rectangle(frame, (x_left,y_top), (x_left+width,y_top+height), (0, 0, 255), 1) 
+        
+        crop_img = frame[y_top:y_top+height, x_left:x_left+width]
+        img_path = save_file_to_tmp(crop_img)
+        print(img_path)
+
+        prediction, index, emotion = predict_image(img_path)
+        os.remove(img_path)
+
+        #Result
+        model_confidence = prediction[index]
+        if model_confidence > 0.75: # Tỷ lệ quá thấp -> người lạ
+            member_name = CLASS_NAMES[index]
+        else:
+            member_name = 'YOU ARE STRANGER'
+
+        member_emotion = emotion
+
+
+        print('reslt:',prediction, index, emotion)
+        frame = cv2.putText(frame,"{:.2f}%".format(model_confidence *100)+ '-' + member_name,(x_left,y_top), cv2.FONT_HERSHEY_SIMPLEX,  0.8, (255,0,0), 1,  cv2.LINE_AA)
+        frame = cv2.putText(frame,member_emotion,(x_left,y_top+20), cv2.FONT_HERSHEY_SIMPLEX,  0.8, (0,255,0), 1,  cv2.LINE_AA)
+    
+    except IndexError as ierr:
+        frame = cv2.putText(frame,'Cannot found Bounding Box',(20,20), cv2.FONT_HERSHEY_SIMPLEX,  0.8, (255,0,0), 1,  cv2.LINE_AA)
+        print(ierr)
+        pass
+
+    except Exception as e:
+        frame = cv2.putText(frame,str(e),(10,10), cv2.FONT_HERSHEY_SIMPLEX,  0.8, (255,0,0), 1,  cv2.LINE_AA)
+        print(e)   
+        pass     
+
+    cv2.imshow('Input', frame)
+    
+    c = cv2.waitKey(1)
+    # Break when pressing ESC
+    #print('Button clicked:',c)
+    if c == 27:
+        run = False
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
+
